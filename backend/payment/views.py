a0aeb798e04319ff2a61b62fdc60a7b4b0df3d79
AghastyGD <augustodomingosvasco@gmail.com>
2023-11-13 09:57:44 -0200

    views_para_conexao_de_address_na_api

--- backend/payment/views.py
+++ backend/payment/views.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Customer
+from .models import Customer, Address
 import braintree
 
 gateway = braintree.BraintreeGateway (
@@ -100,12 +100,103 @@ class ProcessPaymentView(APIView):
                         {'error': 'Failed to create customer'},
                         status=status.HTTP_400_BAD_REQUEST
                     )
+
+            if Address.objects.filter(
+                customer=customer,
+                street_address=street_address,
+                city=city,
+                country=country_name,
+                state_province = state_province,
+                postal_zip_code = postal_zip_code,
+            ).exists():
+                address = Address.objects.get(
+                    customer = customer,
+                    street_address = street_address,
+                    city = city,
+                    country = country_name,
+                    state_province = state_province,
+                    postal_zip_code = postal_zip_code,
+                )
+
+                address_id = address.address_id
+                try:
+                    result = gateway.address.find(customer_id, address_id)
+                except:
+                    result = gateway.address.create({
+                        'customer_id': customer_id,
+                        'first_name': first_name,
+                        'street_address': street_address,
+                        'locality': city,
+                        'region': state_province,
+                        'postal_code': postal_zip_code,
+                        'country_name': country_name,
+                        'country_code_alpha2': country_code
+                    })
+
+                    if result.is_success:
+                        address_id = str(result.address.id)
+
+                        Address.objects.filter(
+                            customer = customer,
+                            street_address = street_address,
+                            city = city,
+                            country = country_name,
+                            state_province = state_province,
+                            postal_zip_code = postal_zip_code
+                        ).update(address_id=address_id)
+                    else:
+                        return Response(
+                            {'error': 'Address invalid'},
+                            status=status.HTTP_400_BAD_REQUEST,
+                        )
+            else:
+                result = gateway.address.create({
+                    'customer_id': customer_id,
+                    'first_name': first_name,
+                    'street_address': street_address,
+                    'locality': city,
+                    'region': state_province,
+                    'postal_code': postal_zip_code,
+                    'country_name': country_name,
+                    'country_code_alpha2': country_code
+                })
+
+                if result.is_success:
+                    address_id = str(result.address.id)
+
+                    Address.objects.create(
+                        customer=customer,
+                        address_id=address_id,   
+                        street_address = street_address,
+                        city = city,
+                        country = country_name,
+                        state_province = state_province,
+                        postal_zip_code = postal_zip_code
+                    )
+
+                    address = Address.objects.get(
+                        customer=customer,
+                        address_id=address_id,   
+                        street_address = street_address,
+                        city = city,
+                        country = country_name,
+                        state_province = state_province,
+                        postal_zip_code = postal_zip_code
+                    )
+                else:
+                    return Response(
+                        {'error': 'Failed to create address'},
+                        status=status.HTTP_400_BAD_REQUEST
+                    )
+
             return Response(
-                {'success': 'Created customer sucessfully'},
+                {'success': 'Created customer and address sucessfully'},
                 status=status.HTTP_201_CREATED
             )
 
-        except:
+
+        except Exception as e:
+            print(e)
             return Response(
                 {'error:' 'Something went wrong when processing payment'},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
